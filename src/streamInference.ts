24f459f2f384f6c8845ca932a87cc73d64315c4c
joshuaaung <joshuayks.aung@gmail.com>
2023-07-11 11:16:33 +0800

    refactor: just yield the chunks from the stream whenever new streams come in

--- src/streamInference.ts
+++ src/streamInference.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-async function* streamInference(prompt: string): AsyncGenerator<string> {
-  const endpoint = "http://localhost:8100/code/explain";
+async function* streamInference(
+  prompt: string,
+  inferenceEndpoint: string
+): AsyncGenerator<string> {
+  const endpoint = inferenceEndpoint;
 
   try {
     const response = await axios.post(
@@ -18,27 +21,28 @@ async function* streamInference(prompt: string): AsyncGenerator<string> {
     let textResponse = "";
 
     for await (const chunk of stream) {
-      textResponse += chunk.toString();
+      yield chunk.toString();
+      //   textResponse += chunk.toString();
 
-      // Check if a complete response has been received
-      if (textResponse.includes("\n")) {
-        const responses = textResponse.split("\n");
+      //   // Check if a complete response has been received
+      //   if (textResponse.includes("\n")) {
+      //     const responses = textResponse.split("\n");
 
-        // Yield each response, except the last one (incomplete)
-        for (let i = 0; i < responses.length - 1; i++) {
-          const response = responses[i];
-          yield response;
-        }
+      //     // Yield each response, except the last one (incomplete)
+      //     for (let i = 0; i < responses.length - 1; i++) {
+      //       const response = responses[i];
+      //       yield response;
+      //     }
 
-        // Store the last incomplete response for the next iteration
-        textResponse = responses[responses.length - 1];
-      }
+      //     // Store the last incomplete response for the next iteration
+      //     textResponse = responses[responses.length - 1];
+      //   }
     }
 
     // Yield the last remaining response
-    if (textResponse) {
-      yield textResponse;
-    }
+    // if (textResponse) {
+    //   yield textResponse;
+    // }
 
     console.log("Stream done");
   } catch (error) {
